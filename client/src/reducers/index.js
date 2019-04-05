1c16e71c5539871af00622bdf23f42ffd80717b7
JerYOMat <jeremy.emata@gmail.com>
2019-04-05 02:54:58 -0400

    reset

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import companies from './companies';
-import users from './users'
-import { combineReducers} from 'redux';
-
-export default combineReducers({
-  companies: companies,
-  users: users
-})
